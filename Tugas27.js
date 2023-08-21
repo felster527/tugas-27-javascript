2c40d349c05c79b2f520d335021362c19faddbcd
felster527 <91239878+felster527@users.noreply.github.com>
2023-08-21 19:51:28 +0700

    Add files via upload

---
+++ Tugas27.js
@@ -0,0 +1,11 @@
+function gabungKata(a,b,c,d){
+    a = String(a)
+    b = String(b)
+    c = String(c)
+    d = String(d)
+
+    console.log(a+b+c+d)
+}
+
+gabungKata("No ","rumah saya ",30,"A")
+gabungKata("Jalan pangeran Tirtayasa No",", ",70,"")
